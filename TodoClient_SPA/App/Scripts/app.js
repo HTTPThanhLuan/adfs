822e8a6c241c4cc81f5ba3f5773e8d2f620a6c56
ThanhLuan <luan.tt87@gmail.com>
2020-06-26 10:57:24 -0500

    Update app.js

--- TodoClient_SPA/App/Scripts/app.js
+++ TodoClient_SPA/App/Scripts/app.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 angular.module('todoApp', ['ngRoute','AdalAngular'])
 .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
 
@@ -21,17 +21,27 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
             "https://azurepsctelecomcom.onmicrosoft.com/TodoListService-NativeDotNet",
 
     };
-
     adalProvider.init(
         {
-            instance: 'https://login.microsoftonline.com/', 
-            tenant: 'azurepsctelecomcom.onmicrosoft.com',
-            clientId: '7510b00a-7c79-4048-b388-4d2fa2c4ec85',
-            extraQueryParameter: 'nux=1',
-            endpoints: endpoints,
+            instance: 'https://win-ocsrisfnhe0.arabitpro.local/', // your STS URL
+            tenant: 'adfs',                      // this should be adfs
+            clientId: 'https://localhost:65168', // your client ID of the,
+            postLogoutRedirectUri:'http://localhost:65168/index.html#/TodoList'
+
             //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
         },
         $httpProvider
-        );
+    );
+    //adalProvider.init(
+    //    {
+    //        instance: 'https://login.microsoftonline.com/', 
+    //        tenant: 'azurepsctelecomcom.onmicrosoft.com',
+    //        clientId: '7510b00a-7c79-4048-b388-4d2fa2c4ec85',
+    //        extraQueryParameter: 'nux=1',
+    //        endpoints: endpoints,
+    //        //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+    //    },
+    //    $httpProvider
+    //    );
    
 }]);
